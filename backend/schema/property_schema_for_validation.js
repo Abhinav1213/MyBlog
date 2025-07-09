bc754c5eb9e8d9de2b31e4f111f93e45c29b71f2
Sushant Singh <111882164+sushant7326@users.noreply.github.com>
2025-07-09 15:30:16 +0530

    fix: input validation zod: code cleanup

---
+++ backend/schema/property_schema_for_validation.js
@@ -0,0 +1,32 @@
+import { z } from "zod";
+
+export const user_id_schema = z
+  .string()
+  .regex(/^\d+$/, "ID must be a positive integer");
+
+export const user_email_schema = z.string().email();
+
+export const user_password_schema = z.string().min(6);
+
+export const user_username_schema = z
+  .string()
+  .min(3)
+  .max(50)
+  .regex(/^[^\s]+$/, "Username can only contain alphanumerals, . and -");
+
+export const posts_id_schema = z
+  .string()
+  .regex(/^\d+$/, "ID must be a positive integer");
+
+export const posts_title_schema = z.string().min(3).max(255);
+
+export const posts_date_schema = z
+  .string()
+  .regex(/^\d{4}-\d{2}-\d{2}$/, "Date must be in YYYY-MM-DD format")
+  .refine((val) => !isNaN(Date.parse(val)), {
+    message: "Invalid date value",
+  });
+
+export const posts_author_schema = user_username_schema;
+
+export const posts_description_schema = z.string().optional();
--- backend/validation/posts.js
+++ backend/validation/posts.js
@@ -1,27 +1,25 @@
 import { z } from "zod";
+import {
+  user_username_schema,
+  posts_id_schema,
+  posts_title_schema,
+  posts_description_schema,
+  posts_date_schema
+} from "../schema/property_schema_for_validation.js";
 
 export const sendPostSchema = z.object({
-  title: z.string().min(3).max(100),
-  des: z.string().optional(),
+  title: posts_title_schema,
+  des: posts_description_schema,
 });
 
 export const postsIDParamSchema = z.object({
-  id: z.string().regex(/^\d+$/, "ID must be a positive integer"),
+  id: posts_id_schema,
 });
 
 export const dateParamSchema = z.object({
-  date: z
-    .string()
-    .regex(/^\d{4}-\d{2}-\d{2}$/, "Date must be in YYYY-MM-DD format")
-    .refine((val) => !isNaN(Date.parse(val)), {
-      message: "Invalid date value",
-    }),
+  date: posts_date_schema
 });
 
 export const usernameParamSchema = z.object({
-  username: z
-    .string()
-    .min(3)
-    .max(50)
-    .regex(/^[^\s]+$/, "Username must not contain spaces"),
+  username: user_username_schema,
 });
